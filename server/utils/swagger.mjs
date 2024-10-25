76d916778a5cd9d7a07d9f0bf3c996c54924109a
Por <Poramaporn2540@gmail.com>
2024-10-25 22:26:05 +0700

    feat(document): setting up swagger for API-documents

---
+++ server/utils/swagger.mjs
@@ -0,0 +1,33 @@
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Swagger definition
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Book Management System",
+    version: "1.0.0",
+  },
+  servers: [
+    { url: "http://localhost:3000", description: "Development Server" },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: [path.resolve(__dirname, "../routes/*.mjs")],
+};
+
+//console.log("swagger: ", options);
+//console.log(__dirname);
+
+// Initialize swagger-jsdoc
+const swaggerSpec = swaggerJsDoc(options);
+
+export { swaggerUi, swaggerSpec };
