d47e6861419297d1185f948e0be1f8d352854048
Por <Poramaporn2540@gmail.com>
2024-10-25 14:00:04 +0700

    feat: add db.mjs and .env for configuration

---
+++ server/utils/db.mjs
@@ -0,0 +1,14 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+//get environment parameter
+dotenv.config({ path: "../.env" });
+
+const connectionPool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 10,
+  idleTimeoutMillis: 30000, //close connection in 30s after idle
+  connectionTimeoutMillis: 2000, //return error in 2s after fail connection
+});
+
+export default connectionPool; //export for routers
