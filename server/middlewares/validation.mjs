0ecffe95dbfc298f4e2f247bf98103b27b8a97a3
Por <Poramaporn2540@gmail.com>
2024-11-01 19:08:27 +0700

    feat(auth): add login and register validation in validation.mjs

--- server/middlewares/validation.mjs
+++ server/middlewares/validation.mjs
@@ -33,7 +33,70 @@ export const bookValidation = [
     .isInt({ min: 1 })
     .withMessage("bookid must be positive integer"),
 ];
-//query parameter
+
+export const registerValidation = [
+  //username validate
+  body("username")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("username is required")
+    .isLength({ min: 3, max: 20 })
+    .withMessage("username must be between 3 and 20 characters long"),
+
+  //email validate
+  body("email")
+    .trim()
+    .normalizeEmail() //convert email to lowercase and remove extra spaces
+    .isEmail()
+    .withMessage("Email address is not valid")
+    .notEmpty()
+    .withMessage("Email address is required"),
+
+  //password validate
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[!@#$%&?*]/)
+    .withMessage("Password must contain at least one special character"),
+];
 //login validation
+export const loginValidation = [
+  body("username")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("username is required")
+    .isString()
+    .withMessage("username must be string"),
+  body("password")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("password is required")
+    .isString()
+    .withMessage("password must be string"),
+];
+
+//comfirm password validate this's for register
+/* body("confirmPassword")
+    .trim()
+    .notEmpty()
+    .withMessage("Confirm Password is required")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password confirmation does not match password");
+      }
+      return true;
+    }), */
 
 export default inputValidation;
